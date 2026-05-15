8e0b9b2f3fb4188da45fbdf763987ba86dbe4cbe
Fahmi Idris <fahmiidrismz@gmail.com>
2026-05-15 09:28:11 +0700

    fix: force light theme app

--- lib/features/auth/presentation/pages/welcome_page.dart
+++ lib/features/auth/presentation/pages/welcome_page.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../app/theme/app_colors.dart';
 import '../../../../app/theme/app_dimensions.dart';
 import '../../../../app/theme/app_text_styles.dart';
 import '../../../../core/widgets/app_pill_button.dart';
-import '../cubit/auth_cubit.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
